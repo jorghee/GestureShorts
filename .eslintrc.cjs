de2ed72effd4070e4d00b69901938e78f046a6ad
Jorghee <jorgeehuarsaya@gmail.com>
2024-10-20 08:59:28 -0500

    Modify syntax rules to the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "@electron-toolkit",
+    "@electron-toolkit/eslint-config-prettier"
+  ],
+
+  rules: {
+    "linebreak-style": 0,
+    "react/prop-types": 0
+  }
+};
