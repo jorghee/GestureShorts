e0540d4f9822808489b713bfb47b9c2a48f8f08e
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-10-24 19:16:12 -0500

    Implementation in gestures - added file isRightPinch

--- src/main/gestures/availableGestures.js
+++ src/main/gestures/availableGestures.js
@@ -1,3 +1,7 @@
 import { isPinch } from "./isPinch.js";
+import { isRightPinch } from "./isRightPinch.js";
 
-export const ag = new Map([["isPinch", isPinch]]);
+export const ag = new Map([
+    ["isPinch", isPinch],
+    ["isRightPinch", isRightPinch]
+]);
