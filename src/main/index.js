0680e2868437f4067a64690643577351df4ae73c
mbarrios28 <mbarriosmed@unsa.edu.pe>
2024-11-04 09:52:47 -0500

    Todo arreglado B)

--- src/main/index.js
+++ src/main/index.js
@@ -64,18 +64,18 @@ app.whenReady().then(() => {
 
   ipcMain.handle("performScreenCapture", async (event, gestureStr, landmarks) => {
     await ac.performScreenCapture(gestureStr, landmarks);
+  });
 
   ipcMain.handle("performRightClick", async (event, gestureStr, landmarks) => {
     await ac.performRightClick(gestureStr, landmarks);
   });
+});
 
   app.on("activate", function () {
     // On macOS it"s common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
-});
-});
 
 // Quit when all windows are closed, except on macOS. There, it"s common
 // for applications and their menu bar to stay active until the user quits
