517ee53adf51a51fead31c793db35bd7a0b5ad35
Jorghee <jorgeehuarsaya@gmail.com>
2024-11-13 22:54:21 -0500

    Add a name mapping to display and name all functions defined for gestures and controls

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,11 +1,31 @@
 import { contextBridge, ipcRenderer } from "electron";
 import { electronAPI } from "@electron-toolkit/preload";
 import ac from "../main/controls/availableControls.js";
+import ag from "../main/gestures/availableGestures.js";
+
+const gesturesD = ["Pinch izquierdo", "Pinch derecho", "Puño"];
+const controlsD = ["Clic izquierdo", "Clic derecho", "Captura de pantalla"];
+
+const gestureDisplayNames = new Map();
+const controlDisplayNames = new Map();
+
+let i = 0;
+for (const [controlStr] of ac.entries()) {
+  controlDisplayNames.set(controlsD[i++], controlStr);
+}
+
+i = 0;
+for (const [gestureStr] of ag.entries()) {
+  gestureDisplayNames.set(gesturesD[i++], gestureStr);
+}
 
 // Custom APIs for renderer
 const api = {
   moveMouse: (handedness, smoothed) =>
-    ipcRenderer.invoke("moveMouse", handedness, smoothed)
+    ipcRenderer.invoke("moveMouse", handedness, smoothed),
+
+  getControlDisplayNames: () => controlDisplayNames,
+  getGestureDisplayNames: () => gestureDisplayNames
 };
 
 for (const [controlStr] of ac.entries()) {
