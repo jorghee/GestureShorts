a45941581b988c42ea763a6ab428c03ba497351d
Jorghee <jorgeehuarsaya@gmail.com>
2024-11-13 22:48:27 -0500

    Use router to route from App to GestureControlMapper and the other way around

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -3,9 +3,24 @@ import "./assets/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GestureControlMapper from "./components/GestureControlMapper";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/mapper"
+          element={
+            <GestureControlMapper
+              getGestureDisplayNames={window.api.getGestureDisplayNames}
+              getControlDisplayNames={window.api.getControlDisplayNames}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
