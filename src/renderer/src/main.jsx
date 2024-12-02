44a3fa409b32f3a8914f174af5b7b6787ba1e5b4
Jorghee <jorgeehuarsaya@gmail.com>
2024-12-01 23:12:06 -0500

    Use the Rehabilitation, Customization and Detection components with Links

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,18 +1,22 @@
-import "./assets/main.css";
-import "./assets/GestureControlMapper.css";
+import "./assets/App.css";
+import "./assets/Rehabilitation.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import GestureControlMapper from "./components/GestureControlMapper";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Rehabilitation from "./components/Rehabilitation";
+import Customization from "./components/Customization";
+import Detection from "./components/Detection";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/mapper" element={<GestureControlMapper />} />
+        <Route path="/rehabilitation" element={<Rehabilitation />} />
+        <Route path="/customization" element={<Customization />} />
+        <Route path="/detection" element={<Detection />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
